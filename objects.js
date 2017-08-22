b658abba0196eab6f8e095651768b9fb7fbe7d6a
Adiyam Nerri <adiyam@gmail.com>
2017-08-22 15:25:32 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  micheal Jackson = 'billie jean',
+  bob marley = 'exodus',
+  steve wonder = 'superstition'
+};
+
+function updatePlaylist(obj, 'artistName', 'songTitle'){
+  obj.artistName = songTitle;
+  return obj;
+}
+
+function removeFromPlaylist(obj, artistName) {
+  delete.obj.artistName;
+}
